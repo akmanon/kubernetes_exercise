64928bde357179f7f08eb19a2667d0367f633682
Ashish Kumar <kashish979@gmail.com>
2024-02-10 13:28:28 +0530

    Added basic Imperative Coomands

--- Creating_Pod_Imperative_way.sh
+++ Creating_Pod_Imperative_way.sh
@@ -1,2 +1,19 @@
 #Create Pod in Imperative Ways
 kubectl run myapp --image=nginx --restart=never
+
+#Get Pods Details
+kubectl get pods -o wide
+
+#Get More Info about the Pods
+kubectl describe pods myapp
+
+#Get logs of the First Pod and to get live logs add "-f" param
+kubectl logs myapp
+
+#To run Command in Pod and to enter into pod user -it with -- bash in the end
+kubectl exec myapp -- "ls; hostname;"
+
+#TO delete Pod 
+kubectl delete pods myapp
+kubectl get pods -o wide
+
