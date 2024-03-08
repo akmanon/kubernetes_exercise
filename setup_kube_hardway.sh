f39a717255a40bb6a432afbba62bf6f563087e03
Ashish Kumar <kashish979@gmail.com>
2024-03-08 17:59:53 +0530

    Create setup_kube_hardway.sh

---
+++ setup_kube_hardway.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#Installing SSL tool to generate the SSL Selft sign certificates for client and server 
+wget -q --show-progress --https-only --timestamping   https://pkg.cfssl.org/R1.2/cfssl_linux-amd64   https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+
+
+
+
