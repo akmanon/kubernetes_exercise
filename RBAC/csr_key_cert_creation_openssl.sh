a1db3e4e9143be5932757a391fdeefe80304e102
Ashish Kumar <kashish979@gmail.com>
2024-03-21 16:08:50 +0000

    Added user creation steps for kubectl

--- RBAC/csr_key_cert_creation_openssl.sh
+++ RBAC/csr_key_cert_creation_openssl.sh
@@ -13,3 +13,27 @@ openssl x509 -req -in app_user.csr -CA /etc/kubernetes/pki/ca.crt -CAkey /etc/ku
 
 #Verify the Cert
 openssl x509 -in app_user.cert -text -noout
+
+#Get the Cluster Name
+kubectl config get-clusters
+
+#Creating the User in Kubernetes Cluster. 
+#Note "@kuberntes" is the name the name of the cluster
+kubectl config set-credentials appuser --client-certificate="app-user.cert" --client-key="app_user.key" --embed-certs=true
+
+#kubectl config set-credentials appuser@kubernetes --client-certificate="app-user.cert" --client-key="app_user.key" --embed-certs=true
+
+#Setting the Context
+kubectl config set-context appuser --cluster=kubernetes  --user=appuser
+
+#kubectl config set-context appuser@kubernetes --cluster=kubernetes  --user=appuser
+
+#Confirm the context creation
+kubectl config get-contexts
+
+#Switching to appuser context
+kubectl config use-context appuser
+
+#To Generate the Kube config for the user
+kubectl config view --minify --raw
+
